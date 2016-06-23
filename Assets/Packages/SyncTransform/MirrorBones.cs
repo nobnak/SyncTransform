9b315ce912b094a4866320e2216fd04c7f9a7c7e
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-06-23 14:04:51 +0900

    no message

---
+++ Assets/Packages/SyncTransform/MirrorBones.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SyncTransformSystem {
+    [RequireComponent(typeof(SkinnedMeshRenderer))]
+    public class MirrorBones : MonoBehaviour {
+        public SkinnedMeshRenderer sourceSkin;
+
+        SkinnedMeshRenderer _attachedSkin;
+
+        void Awake () {
+            _attachedSkin = GetComponent<SkinnedMeshRenderer> ();
+        }
+    	void Update () {
+            if (sourceSkin == null || _attachedSkin == null) {
+                Debug.LogError ("Null");
+                return;
+            }            
+            var s = sourceSkin.bones;
+            var d = _attachedSkin.bones;
+            if (s == null || d == null || s.Length != d.Length) {
+                Debug.LogError ("Incompatible bones");
+                return;
+            }
+
+            for (var i = 0; i < s.Length; i++) {
+                var str = s [i];
+                var dtr = d [i];
+                dtr.localPosition = str.localPosition;
+                dtr.localRotation = str.localRotation;
+                dtr.localScale = str.localScale;
+            }
+    	}
+    }
+}
