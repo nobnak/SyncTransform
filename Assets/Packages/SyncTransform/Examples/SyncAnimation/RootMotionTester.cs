8ba9ac970e74cdccfe10f21f8fd742c996281b2b
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-07-25 19:27:41 +0900

    ルートノード同期のテスト

---
+++ Assets/Packages/SyncTransform/Examples/SyncAnimation/RootMotionTester.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+namespace SyncTransformSystem {
+    public class RootMotionTester : NetworkBehaviour {
+        public const float TWO_PI = 2f * Mathf.PI;
+        public float freq = 0.1f;
+        public float scale = 1f;
+
+    	void Update () {
+            if (isServer) {
+                var t = freq * Time.timeSinceLevelLoad;
+                var angle = TWO_PI * t;
+                transform.localPosition = scale * new Vector3 (Mathf.Cos (angle), Mathf.Sin (angle), 0f);
+            }
+    	}
+    }
+}
